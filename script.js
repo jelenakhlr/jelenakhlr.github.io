7dfbe6b34d7a1fa2117d5ef22ee68979aaee882b
Jelena Petereit <108803123+jelenakhlr@users.noreply.github.com>
2023-07-27 13:47:38 +0200

    sliding tabs

---
+++ script.js
@@ -0,0 +1,22 @@
+// For sliding tabs
+const tabs = document.querySelectorAll('.tab-list li');
+const contentContainers = document.querySelectorAll('#home, #about, #contact');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    // Remove active class from all tabs and content containers
+    tabs.forEach(t => t.classList.remove('active'));
+    contentContainers.forEach(c => c.classList.remove('active'));
+
+    // Add active class to the clicked tab and corresponding content container
+    tab.classList.add('active');
+    contentContainers[index].classList.add('active');
+  });
+});
+
+// For the hamburger menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+});
